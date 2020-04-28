4f6503699f92fe90e9d00e54c6fb3e1ab0ce0f79
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-29 00:12:11 +0530

    top + bottom textfields added

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,21 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var imagePickerView: UIImageView!
     
+    @IBOutlet var cameraButton: UIBarButtonItem!
+    
+    @IBOutlet var topTextField: UITextField!
+    
+    @IBOutlet var bottomTextField: UITextField!
+    
+    let memeTextFieldDelegate = MemeTextFieldDelegate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        topTextField.delegate = memeTextFieldDelegate
+        bottomTextField.delegate = memeTextFieldDelegate
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
     
-    @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
+    @IBAction func pickAnImageFromAlbum(_ sender: UIBarButtonItem) {
          let imagePicker = UIImagePickerController()
          imagePicker.delegate = self
+         imagePicker.sourceType = .photoLibrary
          present(imagePicker, animated: true, completion: nil)
      }
     
+    @IBAction func pickAnImageFromCamera(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+           if let image = info[/* TODO: Dictionary Key Goes Here */] as? UIImage {
+                imagePickerView.image = image
+            }
+    }
 }
 
