72a1c77dfd21e9494b8a099ea1b499f219d4cceb
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-29 00:24:38 +0530

    Meme model added

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Aaryan Kothari on 29/04/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String = "TOP"
+    var bottomText: String = "BOTTOM"
+    var originalImage: UIImage?
+    var memeImage: UIImage?
+}
