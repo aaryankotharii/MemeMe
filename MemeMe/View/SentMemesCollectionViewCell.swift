95de5d4ed36f43a4e57e378d7c1062a024e2f458
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-30 19:26:11 +0530

    SentMemesCollectionViewCell setup done

---
+++ MemeMe/View/SentMemesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Aaryan Kothari on 30/04/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var memeImageView: UIImageView!
+}
